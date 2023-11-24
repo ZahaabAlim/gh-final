744c998b9f711deaa512a40609b07ceb45febc74
ZahaabAlim <Zahaabalim@gmail.com>
2023-11-24 14:45:27 +0530

    new commit

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 #creating s3 bucket
 resource "aws_s3_bucket" "factorial_bucket" {
-  bucket = "zahaab-py-bucket"
+  bucket = "zahaab-pyt-bucket"
   acl= "private"
 }
 #adding object to s3 bucket
