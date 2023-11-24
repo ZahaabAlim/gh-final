8e1ed4e9452f188a868ee335d16c25b68064fc7a
ZahaabAlim <Zahaabalim@gmail.com>
2023-11-24 15:11:18 +0530

    finall

--- main.tf
+++ main.tf
@@ -21,5 +21,5 @@ resource "aws_s3_bucket" "factorial_bucket" {
 resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.factorial_bucket.id
   key    = "factorial.py"
-  source = "C:\\Users\\2130401\\final-task\\factorial.py"
+  source = "C:\\Users\\2130401\\OneDrive - Cognizant\\Desktop\\factorial.py"
 }
